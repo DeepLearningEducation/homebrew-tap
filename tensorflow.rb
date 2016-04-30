11257f1afbee9188cbf91dffc096d1ff002ab6cc
Amund Tveit <atveit@users.noreply.github.com>
2016-04-30 13:44:01 +0200

    Update tensorflow.rb

--- tensorflow.rb
+++ tensorflow.rb
@@ -1,13 +1,1 @@
-class Tensorflow < Formula
-  desc "Simple Script to install Google's TensorFlow with Homebrew"
-  homepage "https://github.com/DeepLearningEducation/tensorflow"
-  url "https://github.com/DeepLearningEducation/tensorflow/archive/0.0.1.tar.gz"
-  sha256 "b83d98cacbb207f77cb136ce2bd32e694856a2063ea80bd1a1574959b600229d"
 
-  def install
-    system date
-  end
-
-  test do
-  end
-end
