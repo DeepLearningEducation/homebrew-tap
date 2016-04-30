61a33925f5c87603bee24cda025953d4d8eddce2
Amund Tveit <atveit@gmail.com>
2016-04-30 12:36:30 +0200

    initial commit of tensorflow install for homebrew

---
+++ tensorflow.rb
@@ -0,0 +1,13 @@
+class Tensorflow < Formula
+  desc "Simple Script to install Google's TensorFlow with Homebrew"
+  homepage "https://github.com/DeepLearningEducation/tensorflow"
+  url "https://github.com/DeepLearningEducation/tensorflow/archive/0.0.1.tar.gz"
+  sha256 "b83d98cacbb207f77cb136ce2bd32e694856a2063ea80bd1a1574959b600229d"
+
+  def install
+    bin.install install_and_run_tensorflow_with_docker.sh
+  end
+
+  test do
+  end
+end
